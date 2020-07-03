cfee049ef50c1c91acf0aa812f1c9a2c83c3b8b2
lin z <linbox2108@gmail.com>
2020-07-03 17:43:39 +0300

    LoginPage for manager and regular user in separate

--- UserInterface/src/LoginPage.java
+++ UserInterface/src/LoginPage.java
@@ -1,3 +1,4 @@
+import enums.ResponseStatus;
 import serviceHost.ServiceCommand;
 import services.requests.LoginRequest;
 import services.responses.LoginResponse;
@@ -43,50 +44,34 @@ public class LoginPage {
                     JOptionPane.showMessageDialog(null,"Please enter password"); //Display dialog box with the message
                 }
                 else {
-                    UserMenu.user_menu("Lin");
-                    AdminMenu.admin_menu();
-                    f.dispose();
                     LoginRequest request = new LoginRequest(username,password);
                     ServiceCommand sc = ServiceCommand.getInstance();
                     LoginResponse resposne = sc.execute(request);
-                    //If both the fields are present then to login the user, check wether the user exists already
-                    //System.out.println("Login connect");
-                   /* Connection connection=connect();  //Connect to the database
-                    try
+                    if(resposne.getStatus() != ResponseStatus.OK.errorCode())
                     {
-                        Statement stmt = connection.createStatement();
-                        stmt.executeUpdate("USE LIBRARY"); //Use the database with the name "Library"
-                        String st = ("SELECT * FROM USERS WHERE USERNAME='"+username+"' AND PASSWORD='"+password+"'"); //Retreive username and passwords from users
-                        ResultSet rs = stmt.executeQuery(st); //Execute query
-                        if(rs.next()==false) { //Move pointer below
-                            System.out.print("No user");
-                            JOptionPane.showMessageDialog(null,"Wrong Username/Password!"); //Display Message
-
-                        }
-                        else {
-                            f.dispose();
-                            rs.beforeFirst();  //Move the pointer above
-                            while(rs.next())
-                            {
-                                String admin = rs.getString("ADMIN"); //user is admin
-                                //System.out.println(admin);
-                                String UID = rs.getString("UID"); //Get user ID of the user
-                                if(admin.equals("1")) { //If boolean value 1
-                                    //admin_menu(); //redirect to admin menu
-                                }
-                                else{
-                                    //user_menu(UID); //redirect to user menu for that user ID
-                                }
-                            }
-                        }
+                        JOptionPane.showMessageDialog(null,"Wrong Username/Password!"); //Display Message
                     }
-                    catch (Exception ex) {
-                        ex.printStackTrace();
-                    }*/
+                    else
+                    {
+                        switch (resposne.getUser().getUserStatus())
+                        {
+                            case 1:
+                                UserMenu.user_menu("");
+                                break;
+                            case 2:
+                                UserMenu.user_menu("");
+                                break;
+                            case 3:
+                                AdminMenu.admin_menu();
+                                break;
+                            default:
+                                break;
+                        }
+                        f.dispose();
+                    }
                 }
             }
         });
-
 
         f.add(F_pass); //add password
         f.add(login_but);//adding button in JFrame
